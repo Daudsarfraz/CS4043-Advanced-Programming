1f0b102a3ba0094e069b43fd39d02e2f4e7dc46f
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2024-08-02 08:25:38 +0500

    upload

--- Assignment 2/calc.py
+++ Assignment 2/calc.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 def frame(root, side):
     w = Frame(root)
-    w.pack(side=side, expand=NO, fill=BOTH)
+    w.pack(side=side, expand=YES, fill=BOTH)
     return w
 
 def button(root, side, text, command=None):
@@ -14,29 +14,27 @@ class Calculator(Frame):
     def __init__(self):
         Frame.__init__(self)
         self.pack(expand=YES, fill=BOTH)
-        self.master.title('Simple Calculator by Dawood Sarfraz')
+        self.master.title('Simple Calculator')
         self.master.iconname("calc1")
         
         display = StringVar()
         Entry(self, relief=SUNKEN, textvariable=display).pack(side=TOP, expand=YES, fill=BOTH)
 
-        opsF = frame(self, RIGHT)
-        for char in "+-*/=":
-            if char == '=':
-                btn = button(opsF, RIGHT, char)
-                btn.bind('<ButtonRelease-1>', lambda e, s=self, w=display: s.calc(w), '+')
-            else:
-                btn = button(opsF, TOP, char, lambda w=display, c=char: w.set(w.get()+' '+c+' '))
-        
-
-        for key in ("123", "456", "789", ",0."):
+        for key in ("123", "456", "789", "-0."):
             keyF = frame(self, TOP)
             for char in key:
                 button(keyF, LEFT, char, lambda w=display, s=' %s '%char: w.set(w.get()+s))
             
+        opsF = frame(self, TOP)
+        for char in "+-*/=":
+            if char == '=':
+                btn = button(opsF, LEFT, char)
+                btn.bind('<ButtonRelease-1>', lambda e, s=self, w=display: s.calc(w), '+')
+            else:
+                btn = button(opsF, LEFT, char, lambda w=display, c=char: w.set(w.get()+' '+c+' '))
         
         clearF = frame(self, BOTTOM)
-        button(clearF, LEFT, 'Clear', lambda w=display: w.set(''))
+        button(clearF, LEFT, 'Clr', lambda w=display: w.set(''))
 
     def calc(self, display):
         try:
